5f38f52c421142eae25c55c19d01f8063071cb60
huagru <prosopisnigra@gmail.com>
2021-12-06 11:40:28 -0300

    especificado que las cajas acepten int y corrección de errores

--- pulverizadoraGUI.py
+++ pulverizadoraGUI.py
@@ -8,6 +8,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self) 
         
         self.boton_calcular.clicked.connect(self.calcular)
+        
+        # Para que las cajan acepten solo números.
+        self.caja_caudal_campo.setValidator(QtGui.QIntValidator()) 
+        self.caja_caudal_boquilla.setValidator(QtGui.QIntValidator())
+        self.caja_velocidad_avance.setValidator(QtGui.QIntValidator())
+        self.caja_ancho_labor_boquilla.setValidator(QtGui.QIntValidator())
+
+
+
+        #  PARECE QUE AUTOMATICAMENTE CONVIERTE LOS NUMEROS A INTEGERES. ME LA QUIERO CORTAR CON UNA PLANCHA.
+        # Probar de sacar TODO lo que mapea y convierte a int.
+
+        
 
 
     
@@ -15,7 +28,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Recibe los datos de las cajas y las agrega a la lista "valores":
         caudal_de_campo = self.caja_caudal_campo.text()
         valores.append(caudal_de_campo)
-                
+
+
         caudal_de_boquilla = self.caja_caudal_boquilla.text()
         valores.append(caudal_de_boquilla)
 
@@ -25,26 +39,27 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         ancho_de_boquilla = self.caja_ancho_labor_boquilla.text()
         valores.append(ancho_de_boquilla)
 
-
         
-        # esto usa map() para iterar por la lista valores y aplica la función convertir() a cada valor. Devuelve una nueva lista mapeados:
+        
+        # Esto usa map() para iterar por la lista valores y aplica la función convertir() a cada valor. Devuelve una nueva lista mapeados:
         mapeados = list(map(convertir, valores))
 
 
+        
 
         # CÁLCULOS:
         # Cálculo del caudal de campo (Q), el primer elemento de la lista "mapeados":
         if mapeados[0] == "x":
             caudalCampo = formulasPulverizadora.Q(mapeados[1], mapeados[2], mapeados[3])
             
-            self.resultados.setText("El caudal de campo es: " + str(caudalCampo))
+            self.resultados.setText("El caudal de campo es: " + str(caudalCampo) + " litros por hectárea")
         
         
         # Cálculo del caudal de boquilla (q), el segundo elemento de la lista "mapeados":    
         elif mapeados[1] == "x":
             caudalBoquilla = formulasPulverizadora.q(mapeados[0], mapeados[2], mapeados[3])
 
-            self.resultados.setText("El caudal de boquilla es: " + str(caudalBoquilla))
+            self.resultados.setText("El caudal de boquilla es: " + str(caudalBoquilla) + " liros por minuto.")
 
 
         # Cálculo de la velocidad de avance (v), el tercer elemento de la lista "mapeados":    
@@ -63,11 +78,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
             
 
-
-        # para probar si todo va en orden:
-        print(mapeados)
-
-        
+    
 
 
 # Función usada por el map(): si hay un "" mete una x, y si hay un número como string, lo pasa a integer:
@@ -82,7 +93,7 @@ def convertir(elemento):
 
 # Lista que recibe los valores de las cajas al apretar el botón calcular:       
 valores = []
-# (recordar que los valores mapeados están en la lista mapeados. Usar esa para los cálculos)    <-----------------
+
         
 
      
