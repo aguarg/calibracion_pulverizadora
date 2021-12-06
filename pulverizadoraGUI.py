f6eb34f3f6e9d4f4139b7d732e30b527c08309c4
aguarg <93261481+aguarg@users.noreply.github.com>
2021-12-06 09:30:39 -0300

    Add files via upload
    Terminadas las ecuaciones y su verificación.

---
+++ pulverizadoraGUI.py
@@ -0,0 +1,96 @@
+from pulverizadora_ui import *
+import formulasPulverizadora
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self) 
+        
+        self.boton_calcular.clicked.connect(self.calcular)
+
+
+    
+    def calcular(self):
+        # Recibe los datos de las cajas y las agrega a la lista "valores":
+        caudal_de_campo = self.caja_caudal_campo.text()
+        valores.append(caudal_de_campo)
+                
+        caudal_de_boquilla = self.caja_caudal_boquilla.text()
+        valores.append(caudal_de_boquilla)
+
+        velocidad = self.caja_velocidad_avance.text()
+        valores.append(velocidad)
+
+        ancho_de_boquilla = self.caja_ancho_labor_boquilla.text()
+        valores.append(ancho_de_boquilla)
+
+
+        
+        # esto usa map() para iterar por la lista valores y aplica la función convertir() a cada valor. Devuelve una nueva lista mapeados:
+        mapeados = list(map(convertir, valores))
+
+
+
+        # CÁLCULOS:
+        # Cálculo del caudal de campo (Q), el primer elemento de la lista "mapeados":
+        if mapeados[0] == "x":
+            caudalCampo = formulasPulverizadora.Q(mapeados[1], mapeados[2], mapeados[3])
+            
+            self.resultados.setText("El caudal de campo es: " + str(caudalCampo))
+        
+        
+        # Cálculo del caudal de boquilla (q), el segundo elemento de la lista "mapeados":    
+        elif mapeados[1] == "x":
+            caudalBoquilla = formulasPulverizadora.q(mapeados[0], mapeados[2], mapeados[3])
+
+            self.resultados.setText("El caudal de boquilla es: " + str(caudalBoquilla))
+
+
+        # Cálculo de la velocidad de avance (v), el tercer elemento de la lista "mapeados":    
+        elif mapeados[2] == "x":
+            velocidadAvance = formulasPulverizadora.v(mapeados[0], mapeados[1], mapeados[3])
+
+            self.resultados.setText("La velocidad de avance es de: " + str(velocidadAvance) + " kilómetros por hora") 
+
+
+
+        # Cálculo del ancho de boquilla (a), el cuarto elemento de la lista "mapeados":    
+        elif mapeados[3] == "x":
+            anchoBoquilla = formulasPulverizadora.a(mapeados[0], mapeados[1], mapeados[2])
+
+            self.resultados.setText("El ancho de labor de cada boquilla es de: " + str(anchoBoquilla) + " metros")           
+
+            
+
+
+        # para probar si todo va en orden:
+        print(mapeados)
+
+        
+
+
+# Función usada por el map(): si hay un "" mete una x, y si hay un número como string, lo pasa a integer:
+def convertir(elemento):
+    if elemento == "":
+        return "x"
+    else:
+        return int(elemento) 
+
+
+
+
+# Lista que recibe los valores de las cajas al apretar el botón calcular:       
+valores = []
+# (recordar que los valores mapeados están en la lista mapeados. Usar esa para los cálculos)    <-----------------
+        
+
+     
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
